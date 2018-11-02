74ebf04001386a380314490a92787f4577f59f03
guanw88 <guanw88@gmail.com>
2018-11-02 15:42:01 -0700

    Create LinkedList Class and associated methods

--- LRU_Cache/lib/p04_linked_list.rb
+++ LRU_Cache/lib/p04_linked_list.rb
@@ -1,4 +1,5 @@
 class Node
+  
   attr_reader :key
   attr_accessor :val, :next, :prev
 
@@ -20,7 +21,13 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
+  
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail 
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,30 +36,75 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    self.each do |node|
+      return node.val if node.key == key
+    end
+    nil
   end
 
   def include?(key)
+    self.each do |node|
+      return true if node.key == key
+    end 
+    
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    
+    last.next = new_node #update last
+    
+    new_node.prev = last #update new node
+    new_node.next = @tail 
+    
+    @tail.prev = new_node #update tail 
   end
 
   def update(key, val)
+    
+    self.each do |node|
+      node.val = val if node.key == key
+    end
+    
   end
 
   def remove(key)
+    current_node = nil
+    self.each do |node|
+      if node.key == key
+        #assigns nodes
+        current_node = node 
+        previous_node = current_node.prev 
+        next_node = current_node.next
+        #re-assigns previous and next to bypass current node
+        previous_node.next = next_node 
+        next_node.prev = previous_node
+        break
+      end 
+    end
   end
 
   def each
+    current_node = first 
+    
+    until current_node == @tail
+      yield(current_node)
+      current_node = current_node.next
+    end 
+
   end
 
   # uncomment when you have `each` working and `Enumerable` included
